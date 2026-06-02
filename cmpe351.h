35412e28f14d7f26b403954446dfbf8504acdcb7
janvier Harerimana <janvierharerimana381@gmail.com>
2026-06-02 14:38:19 +0300

    Create cmpe351.h

---
+++ cmpe351.h
@@ -0,0 +1,11 @@
+#ifndef CMPE351_H
+#define CMPE351_H
+
+// CPU Scheduler Project - Spring 2026
+// Student Name: [PUT YOUR NAME HERE]
+// Student ID: [PUT YOUR ID HERE]
+
+// This header is required for the project
+// Do not remove or modify this file without instructor permission
+
+#endif
